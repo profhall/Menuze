e8d62c319232b9f4acccf6e93549aa3685572702
Professor Hall <iprofhall@gmail.com>
2022-12-08 15:09:05 -0500

    Updates

---
+++ precache-manifest.b4ca1fa546198a907aaa86a26608f58a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe7417b2e1deed186b4f943f06964d3",
+    "url": "/Menuze/index.html"
+  },
+  {
+    "revision": "4a2114657cf9544c85f2",
+    "url": "/Menuze/static/css/main.42b3b8ab.chunk.css"
+  },
+  {
+    "revision": "4a0341a52c4586af76ed",
+    "url": "/Menuze/static/js/2.b5828a75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Menuze/static/js/2.b5828a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2114657cf9544c85f2",
+    "url": "/Menuze/static/js/main.765d6b19.chunk.js"
+  },
+  {
+    "revision": "dbf92bdbe0ecb87eecb8",
+    "url": "/Menuze/static/js/runtime-main.87ad5ef2.js"
+  },
+  {
+    "revision": "9a05009d9d2e35dc4b65fed905d437e2",
+    "url": "/Menuze/static/media/Logo.9a05009d.png"
+  }
+]);
